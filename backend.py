7fe52b51fee89d2f74483f397b87c67365fecbec
unknown <ahmadsameh660@gmail.com>
2022-08-19 18:25:19 +0200

    added controls

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
-from textwrap import shorten
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QMainWindow, QDialog, QFileDialog
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMainWindow, QDialog, QFileDialog, QShortcut
+from PyQt5.QtGui import QImage, QPixmap, QKeySequence
 from frontend import Ui_MainWindow
 from stitch_frontend import Ui_Stitch
 import cv2 as cv
 from stitch import stitch_images
+import threading
+import keyboard
+import serial
 
 class Interface(QMainWindow):
     def __init__(self):
@@ -14,6 +17,45 @@ class Interface(QMainWindow):
         self.ui.setupUi(self)
         self.capture = False
         self.ui.StchBtn.clicked.connect(self.show_stitched)
+        self.on_color = 'rgba( 9, 237, 235, 255)'
+        self.off_color = 'rgba(0, 88, 174, 255)'
+        self.thread = threading.Thread(target=self.connection)
+        #self.thread.start()
+        
+    def connection(self):
+        print('TEST')
+        arduino = serial.Serial(port='COM17', baudrate=9600, timeout=.1)
+        while True:
+            if 1:
+                if keyboard.is_pressed("w") and keyboard.is_pressed("a"):
+                    arduino.write(bytes("wa\n", encoding='utf-8'))
+                    print("wa")
+                elif keyboard.is_pressed("s") and keyboard.is_pressed("a"):
+                    arduino.write(bytes("sa\n", encoding='utf-8'))
+                    print("sa")
+                elif keyboard.is_pressed("w") and keyboard.is_pressed("d"):
+                    arduino.write(bytes("wd\n", encoding='utf-8'))
+                    print("wd")
+                elif keyboard.is_pressed("s") and keyboard.is_pressed("d"):
+                    arduino.write(bytes("sd\n", encoding='utf-8'))
+                    print("sd")
+                elif keyboard.is_pressed("w"):
+                    arduino.write(bytes("w\n", encoding='utf-8'))
+                    print("w")
+                elif keyboard.is_pressed("s"):
+                    arduino.write(bytes("s\n", encoding='utf-8'))
+                    print("s")
+                elif keyboard.is_pressed("a"):
+                    arduino.write(bytes("a\n", encoding='utf-8'))
+                    print("a")
+                elif keyboard.is_pressed("d"):
+                    arduino.write(bytes("d\n", encoding='utf-8'))
+                    print("d")
+                else:
+                    arduino.write(bytes("n\n", encoding='utf-8'))
+                    #self.stop_move_car(self.on_color)
+                    print("n")
+                
         
     def display_image(self, img, window=1):
         qformat = QImage.Format_Indexed8
@@ -33,7 +75,7 @@ class Interface(QMainWindow):
     def capture_video(self):
         cap = cv.VideoCapture(0)
         while cap.isOpened():
-            ret, frame = cap.read()
+            _, frame = cap.read()
             frame = cv.flip(frame, 1)
             self.display_image(frame, 1)
             cv.waitKey()
@@ -42,6 +84,69 @@ class Interface(QMainWindow):
     def show_stitched(self):
         stitched = StitchDlg(self)
         stitched.exec()
+    
+    def left_movement(self, color1):
+        styleSheet = """
+        QFrame{
+	        background-color: qconicalgradient(cx:0.5, cy:0.5, angle:45, stop:0.749 rgba(0, 88, 174, 0), stop:0.75 {COLOR});
+	        border: none;
+        }
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color1)
+        self.ui.Left.setStyleSheet(newStyleSheet)
+    
+    def right_movement(self, color1):
+        styleSheet = """
+        QFrame{
+	        background-color: qconicalgradient(cx:0.5, cy:0.5, angle:225, stop:0.75 {COLOR}, stop:0.749 rgba(0, 0, 0, 0));
+	        border: none;
+        }
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color1)
+        self.ui.Right.setStyleSheet(newStyleSheet)
+        
+    def forward_movement(self, color1):
+        styleSheet = """
+        QFrame{
+        background-color: qconicalgradient(cx:0.5, cy:0.5, angle:315, stop:0.749 rgba(0, 0, 0, 0), stop:0.75 {COLOR});
+	    border: none;
+        }
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color1)
+        self.ui.Up.setStyleSheet(newStyleSheet)    
+    
+    def backward_movement(self, color1):
+        styleSheet = """
+        QFrame{
+	        background-color: qconicalgradient(cx:0.5, cy:0.5, angle:135, stop:0.749 rgba(0, 0, 0, 0), stop:0.75 {COLOR});
+	        border: none;
+        }
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color1)
+        self.ui.Down.setStyleSheet(newStyleSheet)
+        
+    def forward_left_movement(self, color1):
+        styleSheet = """
+        QFrame{
+        	border: none;
+	        background-color: qconicalgradient(cx:0.7, cy:0.1, angle:225, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 {COLOR});
+        }
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color1)
+        self.ui.UpperLeft.setStyleSheet(newStyleSheet)
+    
+    def stop_move_car(self, color):
+        styleSheet = """
+        QFrame{
+	    background-color: {COLOR};
+	    border: none;
+	    border-radius: 30px;
+        }
+
+        """
+        newStyleSheet = styleSheet.replace("{COLOR}", color)
+        self.ui.Center.setStyleSheet(newStyleSheet)
+            
     
 class StitchDlg(QDialog):
     def __init__(self, parent = None):
@@ -75,6 +180,7 @@ class StitchDlg(QDialog):
             btn.setText('FILE SELECTED!')
             if len(self.img_locations) == 5:
                 self.display_stitched()
+                
             
 if __name__ == "__main__":
     import sys
@@ -82,5 +188,6 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     interface = Interface()
     interface.show()
+    interface.thread.start()
     interface.capture_video()
     sys.exit(app.exec_())
--- frontend.py
+++ frontend.py
@@ -21,7 +21,7 @@ class Ui_MainWindow(object):
 "}")
         self.centralwidget.setObjectName("centralwidget")
         self.Cam = QtWidgets.QLabel(self.centralwidget)
-        self.Cam.setGeometry(QtCore.QRect(390, 10, 601, 491))
+        self.Cam.setGeometry(QtCore.QRect(380, 0, 601, 491))
         self.Cam.setStyleSheet("QLabel{\n"
 "    border-radius: 20px;\n"
 "    border: 5px solid #ffbe00;\n"
@@ -122,18 +122,18 @@ class Ui_MainWindow(object):
         self.DirFrm.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.DirFrm.setFrameShadow(QtWidgets.QFrame.Raised)
         self.DirFrm.setObjectName("DirFrm")
-        self.frame_3 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_3.setGeometry(QtCore.QRect(50, 60, 60, 60))
-        self.frame_3.setStyleSheet("QFrame{\n"
+        self.UpperLeft = QtWidgets.QFrame(self.DirFrm)
+        self.UpperLeft.setGeometry(QtCore.QRect(50, 60, 60, 60))
+        self.UpperLeft.setStyleSheet("QFrame{\n"
 "    border: none;\n"
 "    background-color: qconicalgradient(cx:0.7, cy:0.1, angle:225, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 rgba(0, 88, 174, 255));\n"
 "}")
-        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_3.setObjectName("frame_3")
-        self.frame_10 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_10.setGeometry(QtCore.QRect(100, 100, 60, 60))
-        self.frame_10.setStyleSheet("QFrame{\n"
+        self.UpperLeft.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.UpperLeft.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.UpperLeft.setObjectName("UpperLeft")
+        self.Center = QtWidgets.QFrame(self.DirFrm)
+        self.Center.setGeometry(QtCore.QRect(100, 100, 60, 60))
+        self.Center.setStyleSheet("QFrame{\n"
 "    background-color: #0058ae;\n"
 "    border: none;\n"
 "    border-radius: 30px;\n"
@@ -142,12 +142,12 @@ class Ui_MainWindow(object):
 "QFrame:hover{\n"
 "    background-color: #09edeb;\n"
 "}")
-        self.frame_10.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_10.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_10.setObjectName("frame_10")
-        self.frame_5 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_5.setGeometry(QtCore.QRect(150, 60, 60, 60))
-        self.frame_5.setStyleSheet("QFrame{\n"
+        self.Center.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Center.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Center.setObjectName("Center")
+        self.UpperRight = QtWidgets.QFrame(self.DirFrm)
+        self.UpperRight.setGeometry(QtCore.QRect(150, 60, 60, 60))
+        self.UpperRight.setStyleSheet("QFrame{\n"
 "background-color: qconicalgradient(cx:0.3, cy:0.1, angle:135, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 rgba(0, 88, 174, 255));\n"
 "    border: none;\n"
 "}\n"
@@ -155,63 +155,63 @@ class Ui_MainWindow(object):
 "QFrame:hover{\n"
 "    background-color: qconicalgradient(cx:0.3, cy:0.1, angle:135, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 rgba( 9, 237, 235, 255));\n"
 "}")
-        self.frame_5.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_5.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_5.setObjectName("frame_5")
-        self.frame_4 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_4.setGeometry(QtCore.QRect(180, 100, 60, 60))
-        self.frame_4.setStyleSheet("QFrame{\n"
+        self.UpperRight.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.UpperRight.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.UpperRight.setObjectName("UpperRight")
+        self.Right = QtWidgets.QFrame(self.DirFrm)
+        self.Right.setGeometry(QtCore.QRect(180, 100, 60, 60))
+        self.Right.setStyleSheet("QFrame{\n"
 "    background-color: qconicalgradient(cx:0.5, cy:0.5, angle:225, stop:0.75 rgba(0, 88, 174, 255), stop:0.749 rgba(0, 0, 0, 0));\n"
 "    border: none;\n"
 "}")
-        self.frame_4.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_4.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_4.setObjectName("frame_4")
-        self.frame_7 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_7.setGeometry(QtCore.QRect(50, 140, 60, 60))
-        self.frame_7.setStyleSheet("QFrame{\n"
+        self.Right.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Right.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Right.setObjectName("Right")
+        self.LowerLeft = QtWidgets.QFrame(self.DirFrm)
+        self.LowerLeft.setGeometry(QtCore.QRect(50, 140, 60, 60))
+        self.LowerLeft.setStyleSheet("QFrame{\n"
 "    border: none;\n"
 "    background-color: qconicalgradient(cx:0.7, cy:0.9, angle:315, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 rgba(0, 88, 174, 255));\n"
 "}")
-        self.frame_7.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_7.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_7.setObjectName("frame_7")
-        self.frame_9 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_9.setGeometry(QtCore.QRect(150, 140, 60, 60))
-        self.frame_9.setStyleSheet("QFrame{\n"
+        self.LowerLeft.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.LowerLeft.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.LowerLeft.setObjectName("LowerLeft")
+        self.LowerRight = QtWidgets.QFrame(self.DirFrm)
+        self.LowerRight.setGeometry(QtCore.QRect(150, 140, 60, 60))
+        self.LowerRight.setStyleSheet("QFrame{\n"
 "background-color: qconicalgradient(cx:0.3, cy:0.9, angle:45, stop:0.499 rgba(0, 0, 0, 0), stop:0.5 rgba(0, 88, 174, 255));\n"
 "    border: none;\n"
 "}")
-        self.frame_9.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_9.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_9.setObjectName("frame_9")
-        self.frame_6 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_6.setGeometry(QtCore.QRect(20, 100, 60, 60))
-        self.frame_6.setStyleSheet("QFrame{\n"
+        self.LowerRight.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.LowerRight.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.LowerRight.setObjectName("LowerRight")
+        self.Left = QtWidgets.QFrame(self.DirFrm)
+        self.Left.setGeometry(QtCore.QRect(20, 100, 60, 60))
+        self.Left.setStyleSheet("QFrame{\n"
 "    background-color: qconicalgradient(cx:0.5, cy:0.5, angle:45, stop:0.749 rgba(0, 88, 174, 0), stop:0.75 rgba(0, 88, 174, 255));\n"
 "    border: none;\n"
 "}")
-        self.frame_6.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_6.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_6.setObjectName("frame_6")
-        self.frame_2 = QtWidgets.QFrame(self.DirFrm)
-        self.frame_2.setGeometry(QtCore.QRect(100, 20, 60, 60))
-        self.frame_2.setStyleSheet("QFrame{\n"
+        self.Left.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Left.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Left.setObjectName("Left")
+        self.Up = QtWidgets.QFrame(self.DirFrm)
+        self.Up.setGeometry(QtCore.QRect(100, 20, 60, 60))
+        self.Up.setStyleSheet("QFrame{\n"
 "background-color: qconicalgradient(cx:0.5, cy:0.5, angle:315, stop:0.749 rgba(0, 0, 0, 0), stop:0.75 rgba(0, 88, 174, 255));\n"
 "    border: none;\n"
 "}")
-        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame_2.setObjectName("frame_2")
-        self.frame = QtWidgets.QFrame(self.DirFrm)
-        self.frame.setGeometry(QtCore.QRect(100, 180, 60, 60))
-        self.frame.setStyleSheet("QFrame{\n"
+        self.Up.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Up.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Up.setObjectName("Up")
+        self.Down = QtWidgets.QFrame(self.DirFrm)
+        self.Down.setGeometry(QtCore.QRect(100, 180, 60, 60))
+        self.Down.setStyleSheet("QFrame{\n"
 "    background-color: qconicalgradient(cx:0.5, cy:0.5, angle:135, stop:0.749 rgba(0, 0, 0, 0), stop:0.75 rgba(0, 88, 174, 255));\n"
 "    border: none;\n"
 "}")
-        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame.setObjectName("frame")
+        self.Down.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.Down.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.Down.setObjectName("Down")
         self.CapBtn = QtWidgets.QPushButton(self.centralwidget)
         self.CapBtn.setGeometry(QtCore.QRect(40, 400, 121, 81))
         self.CapBtn.setStyleSheet("QPushButton{\n"
@@ -220,7 +220,7 @@ class Ui_MainWindow(object):
 "}")
         self.CapBtn.setText("")
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("capture.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("C:/Users/ahmad/.designer/backup/capture.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.CapBtn.setIcon(icon)
         self.CapBtn.setIconSize(QtCore.QSize(60, 60))
         self.CapBtn.setObjectName("CapBtn")
